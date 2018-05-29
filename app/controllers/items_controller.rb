b408fb8dee1ad064bd281e18bc8c8661d0456a22
Andryhajanirina <andryhaj@gmail.com>
2018-05-29 13:42:46 +0300

    Création action edit update show create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,29 +8,33 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  # def create
-  #   @price = params['price'].to_d
-  #   puts @price
-  #   @item = Item.new(title: params['title'], description: params['description'], price: @price, image_url: params['image_url'])
-  #   if @item.save
-  #     redirect_to "item"
-  #   else
-  #     render "new"
-  #   end
-  # end
+  def create
+    @price = params['price'].to_d
+    puts @price
+    @item = Item.new(title: params['title'], description: params['description'], price: @price, image_url: params['image_url'])
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render "new"
+    end
+  end
 
   def show
     @item = Item.find(params[:id])
   end
 
-  # def update
-  #   @item = Item.find(params[:id])
-  #   @item.update!(item_params)
-  #   redirect_to "item"
-  # end
+  def edit
+    @item = Item.find(params[:id])
+  end
 
-  # private
-  # def item_params
-  #   params.require(:title).permit(:description, :price, :image_url)
-  # end
+  def update
+    @item = Item.find(params[:id])
+    @item.update!(item_params)
+    redirect_to item_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image_url)
+  end
 end
