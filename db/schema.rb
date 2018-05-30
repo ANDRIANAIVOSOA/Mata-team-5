39d3a8d475725470408dbf9bb86b2f2d5cb628bc
ANDRIANAIVOSOA <fenitraandrianaivosoa@gmail.com>
2018-05-30 18:20:00 +0200

    cart ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_29_105417) do
+
+  create_table "carts", force: :cascade do |t|
+    t.string "nom"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "carts_items", id: false, force: :cascade do |t|
+    t.integer "cart_id", null: false
+    t.integer "item_id", null: false
+    t.index ["cart_id", "item_id"], name: "index_carts_items_on_cart_id_and_item_id"
+    t.index ["item_id", "cart_id"], name: "index_carts_items_on_item_id_and_cart_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.integer "cart_id"
+    t.index ["cart_id"], name: "index_users_on_cart_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
